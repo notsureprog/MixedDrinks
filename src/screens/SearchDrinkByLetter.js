0cb21b6efc8033ac42d082e651e8f275f8e6a051
notsureprog <notsureprog@gmail.com>
2023-10-15 22:11:49 -0400

    commit this and come back for detailed error handling. Start setting up a few other pages.

---
+++ src/screens/SearchDrinkByLetter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchDrinkByLetter() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchDrinkByLetter
--- src/screens/SearchDrinkScreen.js
+++ src/screens/SearchDrinkScreen.js
@@ -50,19 +50,77 @@ function SearchDrinkScreen() {
     }
 
     const DrinkResults = ({ main }) => {
-        // let ingredientList;
-        // for (var i = 0; i < data.length; i++) {
-
+        console.log(main)
+        let ingredientIndex;
+        // probably not the best way. maybe split the object
+        // for (var i = 1; i <= 15; i++) {
+        //     console.log(i)
+        //     // if (main.drinks[state.index].strIngredient[i] !== null) {
+        //     //     console.log(main.drinks[state.index].strIngredient[i])
+        //     // } else {
+        //     //     console.log('it is null')
+        //     // }
+        //     ingredientIndex = i
+        //     return ingredientIndex
         // }
+        return (
+            <View>
+                {data !== null && !loading &&
+                    <View>
+                        {state.index >= 0 &&
+                            <View>
+                                <Image style={{ height: 150, width: 150 }} source={{ uri: `${main.drinks[state.index].strDrinkThumb}` }} />
+                                <Text>Drink: {main.drinks[state.index].strDrink}</Text>
+                                <Text>Instructions: {main.drinks[state.index].strInstructions}</Text>
+                                <Text>Ingredients: {main.drinks[state.index].strMeasure} of {main.drinks[state.index].strIngredient}</Text>
+                                <Text>Type of Glass: {main.drinks[state.index].strGlass}</Text>
+                                {state.index === 0 &&
+                                    < View >
+                                        <Pressable onPress={() => dispatch({ type: 'NEXT_DRINK' })}>
+                                            <Text>Next Drink</Text>
+                                        </Pressable>
+                                    </View>
+                                }
+                                {/* state.index === 10 on blue */}
+                                {state.index === main.drinks.length - 1 &&
+                                    <View>
+
+                                        <Pressable onPress={() => dispatch({ type: 'PREV_DRINK' })}>
+                                            <Text>Previous Drink</Text>
+                                        </Pressable>
+                                    </View>
+                                }
+                                {state.index > 0 && state.index <= main.drinks.length - 1 &&
+                                    <View>
+                                        <Pressable onPress={() => dispatch({ type: 'NEXT_DRINK' })}>
+                                            <Text>Next Drink</Text>
+                                        </Pressable>
+                                        <Pressable onPress={() => dispatch({ type: 'PREV_DRINK' })}>
+                                            <Text>Previous Drink</Text>
+                                        </Pressable>
+                                    </View>
+
+                                }
+                            </View>
+                        }
+
+                    </View>
+                }
+            </View >
+        )
+    }
+    if (data !== null) {
+
+        console.log(data.drinks.length)
     }
 
     const renderItem = ({ item }) => (
         <View>
-            {data === null && !loading && view === 'search' &&
+            {data === null && !loading &&
                 <SearchDrink />
             }
-            {data !== null && !loading && view === 'display' &&
-                <DrinkResults />
+            {data !== null && !loading &&
+                <DrinkResults main={item} />
             }
         </View>
 
@@ -78,33 +136,7 @@ function SearchDrinkScreen() {
     }
     return (
         <View>
-            {data !== null && !loading &&
-                <View>
-                    {state.index >= 0 &&
-                        <View>
-                            <Image style={{ height: 150, width: 150 }} source={{ uri: `${data.drinks[state.index].strDrinkThumb}` }} />
-                            <Text>{data.drinks[state.index].strDrink}</Text>
-                            <Text>Instructions: {data.drinks[state.index].strInstructions}</Text>
-                            <Text>Ingredients: {data.drinks[state.index].strMeasure} of {data.drinks[state.index].strIngredient}</Text>
-                            <Pressable onPress={() => dispatch({ type: 'NEXT_DRINK' })}>
-                                <Text>Next Drink</Text>
-                            </Pressable>
-                            <Pressable onPress={() => dispatch({ type: 'PREVIOUS_DRINK' })}>
-                                <Text>Previous Drink</Text>
-                            </Pressable>
-                        </View>
-                    }
-                    {state.index < data.length &&
-                        <View>
-                            <Image style={{ height: 150, width: 150 }} source={{ uri: `${data.drinks[state.index].strDrinkThumb}` }} />
-                            <Text>{data.drinks[state.index].strDrink}</Text>
-                            <Pressable onPress={() => dispatch({ type: 'PREVIOUS_DRINK' })}>
-                                <Text>Previous Drink</Text>
-                            </Pressable>
-                        </View>
-                    }
-                </View>
-            }
+
             {data === null && !loading &&
                 <View>
                     <Text>Hello Search Drink Screen</Text>
@@ -116,9 +148,16 @@ function SearchDrinkScreen() {
             }
             <View>
 
-                {data !== null && !loading && view === 'display' &&
+                {data !== null && !loading &&
                     <View>
-
+                        <TextInput placeholder='Search for a Drink' onChangeText={setSearch} />
+                        <FlatList
+                            data={[data]}
+                            renderItem={renderItem}
+                        />
+                        <Pressable onPress={handleSubmit}>
+                            <Text>Find Drink</Text>
+                        </Pressable>
                     </View>
                 }
             </View>
