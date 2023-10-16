3f04f3b8e70f60de9542f85da59a70b013037be7
notsureprog <notsureprog@gmail.com>
2023-10-15 21:04:32 -0400

    now to work on the functions for renderItem and a little error handling

--- src/screens/SearchDrinkScreen.js
+++ src/screens/SearchDrinkScreen.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import { View, Text, Pressable, TextInput, Image } from 'react-native'
+import { View, Text, Pressable, TextInput, Image, FlatList } from 'react-native'
 import axios from 'axios'
 
 
@@ -17,6 +17,7 @@ function SearchDrinkScreen() {
     const [loading, setLoading] = React.useState(false)
     const [data, setData] = React.useState(null)
     const [state, dispatch] = useReducer(drinkReducer, { index: 0 })
+    const [view, setView] = React.useState('search')
     const GetDrink = async () => {
         if (loading) {
             try {
@@ -34,31 +35,57 @@ function SearchDrinkScreen() {
             }
         }
     }
+    // For future... Very handy but need a little syntax refresher (rendering two or more components in flatlists that is)
+    const SearchDrink = ({ main }) => {
+        return (
+
+            <View>
+                <Text>Hello Search Drink Screen</Text>
+                <TextInput placeholder='Search for a Drink' onChangeText={setSearch} />
+                <Pressable onPress={handleSubmit}>
+                    <Text>Find Drink</Text>
+                </Pressable>
+            </View>
+        )
+    }
+
+    const DrinkResults = ({ main }) => {
+        // let ingredientList;
+        // for (var i = 0; i < data.length; i++) {
+
+        // }
+    }
+
+    const renderItem = ({ item }) => (
+        <View>
+            {data === null && !loading && view === 'search' &&
+                <SearchDrink />
+            }
+            {data !== null && !loading && view === 'display' &&
+                <DrinkResults />
+            }
+        </View>
+
+    )
 
     React.useEffect(() => {
         GetDrink()
-    }, [loading])
+    }, [loading, view])
 
     const handleSubmit = () => {
         setLoading(true)
+
     }
     return (
         <View>
-            {!loading && data === null &&
-                <View>
-                    <Text>Hello Search Drink Screen</Text>
-                    <TextInput placeholder='Search for a Drink' onChangeText={setSearch} />
-                    <Pressable onPress={handleSubmit}>
-                        <Text>Find Drink</Text>
-                    </Pressable>
-                </View>
-            }
-            {!loading && data !== null &&
+            {data !== null && !loading &&
                 <View>
                     {state.index >= 0 &&
                         <View>
                             <Image style={{ height: 150, width: 150 }} source={{ uri: `${data.drinks[state.index].strDrinkThumb}` }} />
                             <Text>{data.drinks[state.index].strDrink}</Text>
+                            <Text>Instructions: {data.drinks[state.index].strInstructions}</Text>
+                            <Text>Ingredients: {data.drinks[state.index].strMeasure} of {data.drinks[state.index].strIngredient}</Text>
                             <Pressable onPress={() => dispatch({ type: 'NEXT_DRINK' })}>
                                 <Text>Next Drink</Text>
                             </Pressable>
@@ -67,12 +94,34 @@ function SearchDrinkScreen() {
                             </Pressable>
                         </View>
                     }
-                    {/* {state.index < something.length} */}
+                    {state.index < data.length &&
+                        <View>
+                            <Image style={{ height: 150, width: 150 }} source={{ uri: `${data.drinks[state.index].strDrinkThumb}` }} />
+                            <Text>{data.drinks[state.index].strDrink}</Text>
+                            <Pressable onPress={() => dispatch({ type: 'PREVIOUS_DRINK' })}>
+                                <Text>Previous Drink</Text>
+                            </Pressable>
+                        </View>
+                    }
                 </View>
             }
-            {loading &&
-                <Text>Loading... Please wait...</Text>
+            {data === null && !loading &&
+                <View>
+                    <Text>Hello Search Drink Screen</Text>
+                    <TextInput placeholder='Search for a Drink' onChangeText={setSearch} />
+                    <Pressable onPress={handleSubmit}>
+                        <Text>Find Drink</Text>
+                    </Pressable>
+                </View>
             }
+            <View>
+
+                {data !== null && !loading && view === 'display' &&
+                    <View>
+
+                    </View>
+                }
+            </View>
         </View>
     )
 }
